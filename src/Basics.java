f96963777806d3782bbd0a33b09b8230778d73c1
MinaMeltem <meltem@gmail.com>
2019-08-04 15:03:12 -0400

    basic methods

--- src/Basics.java
+++ src/Basics.java
@@ -1,8 +1,26 @@
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+
 public class Basics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//BasedURL (Host)
+		
+		RestAssured.baseURI = "https://api.nytimes.com";
+		
+		given().
+				param("q","election").
+				param("api-key","Mx9yFxTIte8DM4XP7gBFQlEWOn4H7WXY").
+				
+				/*header("", "").
+				  cookie("","").
+				  body("")  // POsT body */
+				
+				when().get("svc/search/v2/articlesearch.json").		
+				then().assertThat().statusCode(200);		
 
 	}
 
